3e34eb70ccaa6b1574530e76250e9aa400436fa1
Chupligin Sergey <neochapay@gmail.com>
2021-06-23 14:22:36 +0300

    Add translations

---
+++ translations/glacier-dialer_ru.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>CallLogPage</name>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="35"/>
+        <source>Call log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="73"/>
+        <source>Call journal empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="89"/>
+        <source>long time ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="92"/>
+        <source>days ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="94"/>
+        <source>day ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="98"/>
+        <source>hours ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="100"/>
+        <source>hour ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="103"/>
+        <source>min ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallLogPage.qml" line="106"/>
+        <source>Just now</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CallView</name>
+    <message>
+        <location filename="../src/qml/pages/CallView.qml" line="31"/>
+        <source>Call</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallView.qml" line="94"/>
+        <source>Answer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CallView.qml" line="101"/>
+        <source>Hang up</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ContactDetails</name>
+    <message>
+        <location filename="../src/qml/pages/ContactDetails.qml" line="54"/>
+        <source>First name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/ContactDetails.qml" line="60"/>
+        <source>Last name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/ContactDetails.qml" line="66"/>
+        <source>Phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ContactsPage</name>
+    <message>
+        <location filename="../src/qml/pages/ContactsPage.qml" line="38"/>
+        <source>Contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/ContactsPage.qml" line="48"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialerPage</name>
+    <message>
+        <location filename="../src/qml/pages/DialerPage.qml" line="103"/>
+        <source>Call</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../src/qml/pages/FirstPage.qml" line="35"/>
+        <source>Glacier Dialer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/glacier-dialer.cpp" line="56"/>
+        <source>Dialer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
